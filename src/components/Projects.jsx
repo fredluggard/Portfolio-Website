5c9cc557b8fc5e0fed8d8a436650a1973117118d
Fredluggard <fredrick.ebuka33@gmail.com>
2024-02-18 22:16:53 +0100

    Updated features

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,42 +14,48 @@ function Projects() {
           <div className="project-slide">
             <img src="/images/project-4.jpg" alt="" />
             <div className="project-link">
-              <a href="/">Fred's Recipe </a>
+              <a href="https://todo-app-pi-umber.vercel.app/">Todo App</a>
               <p>Web Design</p>
             </div>
           </div>
           <div className="project-slide1">
-            <img src="/images/project-3.jpg" alt="" />
+            <img src="/images/pet.png" alt="" />
             <div className="project-link">
-              <a href="/">E-Commerce Platform</a>
+              <a href="https://0f83aaab-62a6-492b-9de5-e94a7ef770b7-00-10f0l137h90s4.kirk.replit.dev/">
+                Pet Trading
+              </a>
               <p>Web Design</p>
             </div>
           </div>
           <div className="project-slide2">
-            <img src="/images/project-1.jpg" alt="" />
+            <img src="/images/whisper.png" alt="" />
             <div className="project-link">
-              <a href="/">Whisper Anonymous Messaging </a>
+              <a href="https://whisper-app-three.vercel.app/">
+                Whisper Anonymous Messaging
+              </a>
               <p>Web Design</p>
             </div>
           </div>
           <div className="project-slide3">
-            <img src="/images/project-2.jpg" alt="" />
+            <img src="/images/nolly.png" alt="" />
             <div className="project-link">
-              <a href="/">NollyScore </a>
+              <a href="https://8ae787e1-74e3-42a7-beab-57e02bf356f5-00-6071oeo5avwk.janeway.replit.dev/">
+                NollyScore{" "}
+              </a>
               <p>Web Design</p>
             </div>
           </div>
           <div className="project-slide4">
             <img src="/images/project-6.jpg" alt="" />
             <div className="project-link">
-              <a href="/">Branding & Illustrative Designs </a>
+              <a href="https://fylo-two-zeta.vercel.app/">Fylo</a>
               <p>Web Design</p>
             </div>
           </div>
           <div className="project-slide5">
-            <img src="/images/project-5.jpg" alt="" />
+            <img src="/images/relax.png" alt="" />
             <div className="project-link">
-              <a href="/">Branding & Illustrative Designs </a>
+              <a href="https://relax-animation.vercel.app/">Relax Animation</a>
               <p>Web Design</p>
             </div>
           </div>
